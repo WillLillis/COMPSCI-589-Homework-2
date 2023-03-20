f8d07152447407fc79f6ba40b54ff2c865570395
William Lillis <wlillis@umass.edu>
2023-03-20 18:57:34 -0400

    Initial work

--- src.py
+++ src.py
@@ -7,27 +7,16 @@ from random import randrange
     # I'm going to try to avoid getting caught up in making this code too general and just focus on getting the assignment done
     # Some light optimization/ storing of intermediate values makes sense, but I'll try to not go overboard...
 
-def classify_review(document, ):
-    pass
-
 
 def main():
-    # from the assignment description...
-    train_tot_pos = 12500
-    train_tot_neg = 12500
+    g_laplace_smooth = True # global flag to indicate whether we're using Laplace Smoothing or not
+    g_alpha = 0.1
 
     # programmer's choice...
-    train_pos_perc = 0.2
-    train_neg_perc = 0.2
-    test_pos_perc = 0.2
-    test_neg_perc = 0.2
-    # sanity check...
-    if train_pos_perc + train_neg_perc > 1.0:
-        print(f"ERROR: Bad training percentages: ({train_pos_perc +  train_neg_perc})")
-        return
-    if test_pos_perc + test_neg_perc > 1.0:
-        print(f"ERROR: Bad testing percentages: ({test_pos_perc +  test_neg_perc})")
-        return
+    train_pos_perc = 1.0
+    train_neg_perc = 1.0
+    test_pos_perc = 1.0
+    test_neg_perc = 1.0
 
     (pos_train, neg_train, vocab) = load_training_set(train_pos_perc, train_neg_perc)
     (pos_test,  neg_test)         = load_test_set(test_pos_perc, test_neg_perc)
@@ -71,8 +60,10 @@ def main():
     #for word in vocab:
     #    print(f"{word}: # pos: {vocab_counts[word][0]}, # neg: {vocab_counts[word][1]}")
 
-    # try laplace smoothing???
-
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
     num_corr = 0
     num_incorr = 0
     num_test = 0
@@ -84,27 +75,41 @@ def main():
         pos_prob = numpy.longdouble(prior_prob_pos) # Pr(y_0)
         neg_prob = numpy.longdouble(prior_prob_neg) # Pr(y_1)
         for word in doc:
-            if True: #word not in used: # avoid double counting...
-                used[word] = True 
-                if word in vocab_counts:
-                    if vocab_counts[word][0] > 0:
-                        pos_prob *= numpy.longdouble(vocab_counts[word][0] / vocab_total_pos) # Product: Pr(w_k | y_0) 
-                    if vocab_counts[word][1] > 0:
-                        neg_prob *= numpy.longdouble(vocab_counts[word][1] / vocab_total_neg) # Product: Pr(w_k | y_1)
-                # if the word wasn't in our training set, ignore it (https://campuswire.com/c/G78D7CCD1/feed/174)
+            if g_laplace_smooth == True:
+                if word not in used: # avoid double counting...
+                    used[word] = True 
+                    if word in vocab_counts:
+                        pos_prob *= numpy.longdouble((vocab_counts[word][0] + g_alpha) / (vocab_total_pos + (g_alpha * len(vocab)))) # Product: Pr(w_k | y_0) 
+                        neg_prob *= numpy.longdouble((vocab_counts[word][1] + g_alpha) / (vocab_total_neg + (g_alpha * len(vocab)))) # Product: Pr(w_k | y_1)
+                    else:
+                        pos_prob *= numpy.longdouble(g_alpha / (vocab_total_pos + (g_alpha * len(vocab)))) # Product: Pr(w_k | y_0) 
+                        neg_prob *= numpy.longdouble(g_alpha / (vocab_total_neg + (g_alpha * len(vocab)))) # Product: Pr(w_k | y_1)
+            else:
+                if word not in used: # avoid double counting...
+                    used[word] = True 
+                    if word in vocab_counts:
+                        if vocab_counts[word][0] > 0:
+                            pos_prob *= numpy.longdouble(vocab_counts[word][0] / vocab_total_pos) # Product: Pr(w_k | y_0) 
+                        if vocab_counts[word][1] > 0:
+                            neg_prob *= numpy.longdouble(vocab_counts[word][1] / vocab_total_neg) # Product: Pr(w_k | y_1)
+                    # if the word wasn't in our training set, ignore it (https://campuswire.com/c/G78D7CCD1/feed/174)
         #print(f"pos prob: {pos_prob}, neg_prob: {neg_prob}")
         if pos_prob > neg_prob:
             #print("Classification: POSITIVE")
             num_corr += 1
+            true_positive += 1
         elif pos_prob < neg_prob: 
             #print("Classification: NEGATIVE")
             num_incorr += 1
+            false_negative += 1
         else: # probabiltiies most likely got smashed down to 0...flip a coin
             #print(f"\tHeyo: {pos_prob}, {neg_prob}")
             if randrange(0,2) == 0:
                 num_corr += 1
+                true_positive += 1
             else: 
                 num_incorr += 1
+                false_negative += 1
     print(f"\tPositive score: {num_corr / num_test}")
 
     num_corr = 0
@@ -117,30 +122,50 @@ def main():
         pos_prob = numpy.longdouble(prior_prob_pos) # Pr(y_0)
         neg_prob = numpy.longdouble(prior_prob_neg) # Pr(y_1)
         for word in doc:
-            if True: #word not in used: # avoid double counting...
-                used[word] = True 
-                if word in vocab_counts:
-                    if vocab_counts[word][0] > 0: # if the word wasn't in our training set, ignore it (https://campuswire.com/c/G78D7CCD1/feed/174)
-                        pos_prob *= numpy.longdouble(vocab_counts[word][0] / vocab_total_pos) # Product: Pr(w_k | y_0) 
-                    if vocab_counts[word][1] > 0:
-                        neg_prob *= numpy.longdouble(vocab_counts[word][1] / vocab_total_neg) # Product: Pr(w_k | y_1)
-        #print(f"pos prob: {pos_prob}, neg_prob: {neg_prob}")
+            if g_laplace_smooth == True:
+                 if word not in used: # avoid double counting...
+                    used[word] = True 
+                    if word in vocab_counts:
+                        pos_prob *= numpy.longdouble((vocab_counts[word][0] + g_alpha) / (vocab_total_pos + (g_alpha * len(vocab)))) # Product: Pr(w_k | y_0) 
+                        neg_prob *= numpy.longdouble((vocab_counts[word][1] + g_alpha) / (vocab_total_neg + (g_alpha * len(vocab)))) # Product: Pr(w_k | y_1)
+                    else:
+                        pos_prob *= numpy.longdouble(g_alpha / (vocab_total_pos + (g_alpha * len(vocab)))) # Product: Pr(w_k | y_0) 
+                        neg_prob *= numpy.longdouble(g_alpha / (vocab_total_neg + (g_alpha * len(vocab)))) # Product: Pr(w_k | y_1)
+            else:
+                if word not in used: # avoid double counting...
+                    used[word] = True 
+                    if word in vocab_counts:
+                        if vocab_counts[word][0] > 0: # if the word wasn't in our training set, ignore it (https://campuswire.com/c/G78D7CCD1/feed/174)
+                            pos_prob *= numpy.longdouble(vocab_counts[word][0] / vocab_total_pos) # Product: Pr(w_k | y_0) 
+                        if vocab_counts[word][1] > 0:
+                            neg_prob *= numpy.longdouble(vocab_counts[word][1] / vocab_total_neg) # Product: Pr(w_k | y_1)
+            #print(f"pos prob: {pos_prob}, neg_prob: {neg_prob}")
         if pos_prob > neg_prob:
             #print("Classification: POSITIVE")
             num_incorr += 1
+            false_positive += 1
         elif pos_prob < neg_prob: 
             #print("Classification: NEGATIVE")
             num_corr += 1
+            true_negative += 1
         else: # probabiltiies most likely got smashed down to 0...flip a coin
             #print(f"\tHeyo: {pos_prob}, {neg_prob}")
             if randrange(0,2) == 0:
                 num_incorr += 1
+                false_positive += 1
             else: 
                 num_corr += 1
+                true_negative += 1
     print(f"\tNegative score: {num_corr / num_test}")
+    print(f"TP: {true_positive}, TN: {true_negative}, FP: {false_positive}, FN: {false_negative}")
+    print(f"Confusion-based accuracy: {(true_positive + true_negative) / (true_positive + true_negative + false_positive + false_negative)}")
     print("Log TRICK:")
     print(f"\tPOSITIVE TEST:")
 
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
     num_corr = 0
     num_incorr = 0
     num_test = 0
@@ -150,27 +175,43 @@ def main():
         pos_prob = numpy.longdouble(log2(prior_prob_pos)) # Pr(y_0)
         neg_prob = numpy.longdouble(log2(prior_prob_neg)) # Pr(y_1)
         for word in doc:
-            if word not in used: # avoid double counting...
-                used[word] = True 
-                if word in vocab_counts:
-                    if vocab_counts[word][0] > 0:
-                        pos_prob += log2(numpy.longdouble(vocab_counts[word][0] / vocab_total_pos)) # Product: Pr(w_k | y_0) 
-                    if vocab_counts[word][1] > 0:
-                        neg_prob += log2(numpy.longdouble(vocab_counts[word][1] / vocab_total_neg)) # Product: Pr(w_k | y_1)
-                # if the word wasn't in our training set, ignore it (https://campuswire.com/c/G78D7CCD1/feed/174)
+            if g_laplace_smooth == True:
+                if word not in used: # avoid double counting...
+                    used[word] = True 
+                    if word in vocab_counts:
+                        if vocab_counts[word][0] > 0:
+                            pos_prob += log2(numpy.longdouble((vocab_counts[word][0] + g_alpha) / (vocab_total_pos + (g_alpha * len(vocab))))) # Product: Pr(w_k | y_0) 
+                        if vocab_counts[word][1] > 0:
+                            neg_prob += log2(numpy.longdouble((vocab_counts[word][1] + g_alpha) / (vocab_total_neg + (g_alpha * len(vocab))))) # Product: Pr(w_k | y_1)
+                    else:
+                        pos_prob += log2(numpy.longdouble(g_alpha / (vocab_total_pos + (g_alpha * len(vocab))))) # Product: Pr(w_k | y_0)
+                        neg_prob += log2(numpy.longdouble(g_alpha / (vocab_total_neg + (g_alpha * len(vocab))))) # Product: Pr(w_k | y_1)
+            else:
+                if word not in used: # avoid double counting...
+                    used[word] = True 
+                    if word in vocab_counts:
+                        if vocab_counts[word][0] > 0:
+                            pos_prob += log2(numpy.longdouble(vocab_counts[word][0] / vocab_total_pos)) # Product: Pr(w_k | y_0) 
+                        if vocab_counts[word][1] > 0:
+                            neg_prob += log2(numpy.longdouble(vocab_counts[word][1] / vocab_total_neg)) # Product: Pr(w_k | y_1)
+                    # if the word wasn't in our training set, ignore it (https://campuswire.com/c/G78D7CCD1/feed/174)
         #print(f"pos prob: {pos_prob}, neg_prob: {neg_prob}")
         if pos_prob > neg_prob:
             #print("Classification: POSITIVE")
             num_corr += 1
+            true_positive += 1
         elif pos_prob < neg_prob: 
             #print("Classification: NEGATIVE")
             num_incorr += 1
+            false_negative += 1
         else: # probabiltiies most likely got smashed down to 0...flip a coin
             #print(f"\tHeyo: {pos_prob}, {neg_prob}")
             if randrange(0,2) == 0:
                 num_corr += 1
+                true_positive += 1
             else: 
                 num_incorr += 1
+                false_negative += 1
     print(f"\tPositive score: {num_corr / num_test}")
 
     num_corr = 0
@@ -183,27 +224,44 @@ def main():
         pos_prob = numpy.longdouble(log2(prior_prob_pos)) # Pr(y_0)
         neg_prob = numpy.longdouble(log2(prior_prob_neg)) # Pr(y_1)
         for word in doc:
-            if word not in used: # avoid double counting...
-                used[word] = True 
-                if word in vocab_counts:
-                    if vocab_counts[word][0] > 0: # if the word wasn't in our training set, ignore it (https://campuswire.com/c/G78D7CCD1/feed/174)
-                        pos_prob += log2(numpy.longdouble(vocab_counts[word][0] / vocab_total_pos)) # Product: Pr(w_k | y_0) 
-                    if vocab_counts[word][1] > 0:
-                        neg_prob += log2(numpy.longdouble(vocab_counts[word][1] / vocab_total_neg)) # Product: Pr(w_k | y_1)
+            if g_laplace_smooth == True:
+                 if word not in used: # avoid double counting...
+                    used[word] = True 
+                    if word in vocab_counts:
+                        if vocab_counts[word][0] > 0:
+                            pos_prob += log2(numpy.longdouble((vocab_counts[word][0] + g_alpha) / (vocab_total_pos + (g_alpha * len(vocab))))) # Product: Pr(w_k | y_0) 
+                        if vocab_counts[word][1] > 0:
+                            neg_prob += log2(numpy.longdouble((vocab_counts[word][1] + g_alpha) / (vocab_total_neg + (g_alpha * len(vocab))))) # Product: Pr(w_k | y_1)
+                    else:
+                        pos_prob += log2(numpy.longdouble(g_alpha / (vocab_total_pos + (g_alpha * len(vocab))))) # Product: Pr(w_k | y_0)
+                        neg_prob += log2(numpy.longdouble(g_alpha / (vocab_total_neg + (g_alpha * len(vocab))))) # Product: Pr(w_k | y_1)
+            else:
+                if word not in used: # avoid double counting...
+                    used[word] = True 
+                    if word in vocab_counts:
+                        if vocab_counts[word][0] > 0: # if the word wasn't in our training set, ignore it (https://campuswire.com/c/G78D7CCD1/feed/174)
+                            pos_prob += log2(numpy.longdouble(vocab_counts[word][0] / vocab_total_pos)) # Product: Pr(w_k | y_0) 
+                        if vocab_counts[word][1] > 0:
+                            neg_prob += log2(numpy.longdouble(vocab_counts[word][1] / vocab_total_neg)) # Product: Pr(w_k | y_1)
         #print(f"pos prob: {pos_prob}, neg_prob: {neg_prob}")
         if pos_prob > neg_prob:
             #print("Classification: POSITIVE")
             num_incorr += 1
+            false_positive += 1
         elif pos_prob < neg_prob: 
             #print("Classification: NEGATIVE")
             num_corr += 1
+            true_negative += 1
         else: # probabiltiies most likely got smashed down to 0...flip a coin
             #print(f"\tHeyo: {pos_prob}, {neg_prob}")
             if randrange(0,2) == 0:
                 num_incorr += 1
+                false_positive += 1
             else: 
                 num_corr += 1
+                true_negative += 1
     print(f"\tNegative score: {num_corr / num_test}")
+    print(f"TP: {true_positive}, TN: {true_negative}, FP: {false_positive}, FN: {false_negative}")
 
 
 
